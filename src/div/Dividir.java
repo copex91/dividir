ab53c4ee44112ebeb8b81c0e7796354d5cfa979d
copex91 <cope_1091@hotmail.com>
2017-01-11 12:11:25 +0100

    Commit inicial
    Commit inicial con 1 test

---
+++ src/div/Dividir.java
@@ -0,0 +1,11 @@
+package div;
+
+public class Dividir {
+
+
+	public static int div(int a, int b) {
+		return a/b;
+	}
+		
+
+}
---
+++ test/div/TestDiv.java
@@ -0,0 +1,13 @@
+package div;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDiv {
+
+	@Test
+	public void testDiv1() {
+		assertEquals(4, Dividir.div(8,2));
+	}
+}
